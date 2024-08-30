a42974bea41a8d3705dda466bc35149400ead47e
Scheggetta <81247624+Scheggetta@users.noreply.github.com>
2024-08-30 21:30:44 +0200

    Implemented training methods of base models

--- load_data.py
+++ load_data.py
@@ -1,78 +1,155 @@
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-
-from fairlearn.datasets import fetch_diabetes_hospital
-
 import lightgbm as lgb
+from catboost import Pool
 
 
-def load_diabetes(return_nunique=False):
+def split_data(data, target_name, sensitive_feature_name):
     """
-    This load function automatically prepares the Diabetes 130 Hospitals Dataset (Fairlearn version) for the training
-    processes of our algorithms.
     :return: the dataset splits (train 70%, validation 15%, test 15%) in the expected format
     """
-
-    # TODO: remember to handle the categorical features in LightGBM-based models
-
-    data = fetch_diabetes_hospital().frame
-    data.drop(columns=['readmitted', 'readmit_binary'], inplace=True)
     data_nunique = data.nunique()
 
     data_points = len(data)
     test_size = int(0.15 * data_points)
 
     X_train, X_test = train_test_split(data, test_size=test_size, random_state=42, shuffle=True,
-                                       stratify=data['race'])
+                                       stratify=data[sensitive_feature_name])
 
     X_train, X_val = train_test_split(X_train, test_size=test_size, random_state=42, shuffle=True,
-                                      stratify=X_train['race'])
+                                      stratify=X_train[sensitive_feature_name])
 
-    y_train = X_train['readmit_30_days']
-    y_val = X_val['readmit_30_days']
-    y_test = X_test['readmit_30_days']
+    y_train = X_train[target_name]
+    y_val = X_val[target_name]
+    y_test = X_test[target_name]
 
-    X_train = X_train.drop(columns=['readmit_30_days']).reset_index(drop=True)
-    X_val = X_val.drop(columns=['readmit_30_days']).reset_index(drop=True)
-    X_test = X_test.drop(columns=['readmit_30_days']).reset_index(drop=True)
+    X_train = X_train.drop(columns=[target_name]).reset_index(drop=True)
+    X_val = X_val.drop(columns=[target_name]).reset_index(drop=True)
+    X_test = X_test.drop(columns=[target_name]).reset_index(drop=True)
     y_train = y_train.reset_index(drop=True)
     y_val = y_val.reset_index(drop=True)
     y_test = y_test.reset_index(drop=True)
 
-    if return_nunique:
-        return X_train, y_train, X_val, y_val, X_test, y_test, data_nunique
-    else:
-        return X_train, y_train, X_val, y_val, X_test, y_test
+    return X_train, y_train, X_val, y_val, X_test, y_test, data_nunique
 
 
-def load_diabetes_gbm():
-    X_train, y_train, X_val, y_val, X_test, y_test, data_nunique = load_diabetes(return_nunique=True)
+def convert_to_cat_datasets(data, target_name, sensitive_feature_name, categorical_columns):
+    X_train, y_train, X_val, y_val, X_test, y_test, data_nunique = \
+        split_data(data, target_name, sensitive_feature_name)
 
-    def convert_categorical_to_int(df, categorical_columns):
+    train_data = Pool(X_train, label=y_train, cat_features=categorical_columns)
+    val_data = Pool(X_val, label=y_val, cat_features=categorical_columns)
+    test_data = Pool(X_test, label=y_test, cat_features=categorical_columns)
+
+    return {
+        'raw_data': {'train': X_train, 'val': X_val, 'test': X_test},
+        'nunique': data_nunique,
+        'cat_cols': categorical_columns,
+        'train': train_data,
+        'val': val_data,
+        'test': test_data,
+        'target_name': target_name,
+        'sensitive_feature_name': sensitive_feature_name,
+        'sf_name': sensitive_feature_name,
+        'sf_train': X_train[sensitive_feature_name],
+        'sf_val': X_val[sensitive_feature_name],
+        'sf_test': X_test[sensitive_feature_name]
+    }
+
+
+def convert_to_gbm_datasets(data):
+    X_train = data['raw_data']['train']
+    X_val = data['raw_data']['val']
+    X_test = data['raw_data']['test']
+    y_train = data['train'].get_label()
+    y_val = data['val'].get_label()
+    y_test = data['test'].get_label()
+    data_nunique = data['nunique']
+    categorical_columns = data['cat_cols']
+
+    def convert_categorical_to_int(dfs: dict, categorical_columns):
+        df = pd.concat([dfs['train'], dfs['val'], dfs['test']], axis=0).reset_index(drop=True)
+
         label_encoders = {}
         for col in categorical_columns:
             le = LabelEncoder()
-            df[col] = le.fit_transform(df[col])
+            le.fit(df[col])
             label_encoders[col] = le
-        return df, label_encoders
+        return label_encoders
 
-    categorical_columns = ['race', 'gender', 'age', 'discharge_disposition_id', 'admission_source_id',
-                           'medical_specialty', 'primary_diagnosis', 'max_glu_serum', 'A1Cresult', 'insulin', 'change',
-                           'diabetesMed', 'medicare', 'medicaid', 'had_emergency', 'had_inpatient_days',
-                           'had_outpatient_days']
-    X_train, label_encoders = convert_categorical_to_int(X_train, categorical_columns)
+    label_encoders = convert_categorical_to_int(data['raw_data'], categorical_columns)
 
     for cat in categorical_columns:
         assert len(label_encoders[cat].classes_) == data_nunique[cat]
 
+        X_train[cat] = label_encoders[cat].transform(X_train[cat])
         X_val[cat] = label_encoders[cat].transform(X_val[cat])
         X_test[cat] = label_encoders[cat].transform(X_test[cat])
 
     train_data = lgb.Dataset(X_train, label=y_train, feature_name=list(X_train.columns),
-                             categorical_feature=categorical_columns)
+                             categorical_feature=categorical_columns, free_raw_data=False).construct()
     val_data = lgb.Dataset(X_val, label=y_val, feature_name=list(X_val.columns),
-                           categorical_feature=categorical_columns, reference=train_data)
+                           categorical_feature=categorical_columns, reference=train_data,
+                           free_raw_data=False).construct()
     test_data = lgb.Dataset(X_test, label=y_test, feature_name=list(X_test.columns),
-                            categorical_feature=categorical_columns)
+                            categorical_feature=categorical_columns, free_raw_data=False).construct()
 
-    return train_data, val_data, test_data
+    data['train'] = train_data
+    data['val'] = val_data
+    data['test'] = test_data
+
+    return data
+
+
+def load_diabetes_easy_cat():
+    data = pd.read_csv('datasets/diabetes_easy.csv')
+
+    target_name = 'diabetes'
+    sensitive_feature_name = 'race'
+    categorical_columns = ['gender', 'location', 'hypertension', 'heart_disease', 'smoking_history', 'race']
+
+    old_race_features = ['race:AfricanAmerican', 'race:Asian', 'race:Caucasian', 'race:Hispanic', 'race:Other']
+    to_drop = old_race_features + ['year']
+    data['race'] = data[old_race_features].idxmax(axis=1).apply(lambda x: x.replace('race:', ''))
+    data.drop(columns=to_drop, inplace=True)
+
+    return convert_to_cat_datasets(data, target_name, sensitive_feature_name, categorical_columns)
+
+
+def load_diabetes_hard_cat():
+    """
+    Source: https://archive.ics.uci.edu/dataset/296/diabetes+130-us+hospitals+for+years+1999-2008
+    """
+    data = pd.read_csv('datasets/diabetes_hard.csv')
+
+    target_name = 'readmitted'
+    sensitive_feature_name = 'race'
+    categorical_columns = ['race', 'gender', 'age', 'admission_type_id', 'discharge_disposition_id',
+                           'admission_source_id', 'payer_code', 'medical_specialty', 'diag_1', 'diag_2', 'diag_3',
+                           'max_glu_serum', 'A1Cresult', 'metformin', 'repaglinide', 'nateglinide', 'chlorpropamide',
+                           'glimepiride', 'acetohexamide', 'glipizide', 'glyburide', 'tolbutamide', 'pioglitazone',
+                           'rosiglitazone', 'acarbose', 'miglitol', 'troglitazone', 'tolazamide', 'examide',
+                           'citoglipton', 'insulin', 'glyburide-metformin', 'glipizide-metformin',
+                           'glimepiride-pioglitazone', 'metformin-rosiglitazone', 'metformin-pioglitazone', 'change',
+                           'diabetesMed']
+
+    # Convert target into binary
+    data['readmitted'] = data['readmitted'].apply(lambda x: 0 if x == 'NO' else 1)
+
+    data['max_glu_serum'] = data['max_glu_serum'].fillna('?')
+    data['A1Cresult'] = data['A1Cresult'].fillna('?')
+    to_drop = ['encounter_id', 'patient_nbr', 'weight']
+    data.drop(columns=to_drop, inplace=True)
+
+    return convert_to_cat_datasets(data, target_name, sensitive_feature_name, categorical_columns)
+
+
+def load_diabetes_easy_gbm():
+    data = load_diabetes_easy_cat()
+    return convert_to_gbm_datasets(data)
+
+
+def load_diabetes_hard_gbm():
+    data = load_diabetes_hard_cat()
+    return convert_to_gbm_datasets(data)
--- training.py
+++ training.py
@@ -1,1 +1,89 @@
+from time import time
+import pandas as pd
 import lightgbm as lgb
+from catboost import CatBoostClassifier
+from fairlearn.metrics import MetricFrame
+from fairlearn.metrics import count, selection_rate, false_positive_rate, false_negative_rate, equalized_odds_difference
+from sklearn.metrics import precision_score, accuracy_score, recall_score, f1_score
+import matplotlib.pyplot as plt
+
+from load_data import load_diabetes_easy_cat, load_diabetes_easy_gbm, load_diabetes_hard_cat, load_diabetes_hard_gbm
+
+# Set the display format for floating point numbers to 4 decimal places
+pd.options.display.float_format = '{:.4f}'.format
+
+# TODO:
+#  - training test with FairGBM
+#  - implementing load functions for folktables
+#  - training tests with all models
+
+if __name__ == '__main__':
+    # --- START LightGBM ---
+
+    # data = load_diabetes_hard_gbm()
+    #
+    # lightgbm_params = {
+    #     'num_iterations': 100,
+    #     'objective': 'binary',
+    #     'device_type': 'cpu',
+    #     'num_threads': 8,
+    #     'seed': 42,
+    #     'deterministic': 'true'
+    # }
+    #
+    # start = time()
+    # lightgbm_model = lgb.train(lightgbm_params, data['train'], valid_sets=[data['val']])
+    # end = time()
+    #
+    # y_pred_proba = lightgbm_model.predict(data['raw_data']['test'])
+    # # Convert probabilities to class labels
+    # y_pred = (y_pred_proba > 0.5).astype(int)
+
+    # --- END LightGBM ---
+
+    # --- START CatBoost ---
+
+    data = load_diabetes_hard_cat()
+
+    model = CatBoostClassifier(iterations=100, random_seed=42, verbose=False)
+
+    start = time()
+    model.fit(data['train'], eval_set=data['val'])
+    end = time()
+    y_pred = model.predict(data['test'])
+
+    # --- END CatBoost ---
+
+    y_true = data['test'].get_label()
+    sensitive_features = data['sf_test']
+
+    metrics = {
+        'count': count,
+        'sel': selection_rate,
+        'pre': precision_score,
+        'acc': accuracy_score,
+        'rec/tpr': recall_score,
+        'fnr': false_negative_rate,
+        'fpr': false_positive_rate
+    }
+
+    mf = MetricFrame(
+        metrics=metrics,
+        y_true=y_true,
+        y_pred=y_pred,
+        sensitive_features=sensitive_features
+    )
+
+    mf.by_group.plot.bar(
+        subplots=True,
+        figsize=[8, 18]
+    )
+    plt.show()
+
+    print(mf.overall)
+    print(mf.by_group)
+
+    print('Equalized odds difference (the lower the better): ',
+          equalized_odds_difference(y_true, y_pred, sensitive_features=sensitive_features))
+    print('F1 score: ', f1_score(y_true, y_pred))
+    print('Training time: ', end - start)
